3d5dcf2fa754cdfd2b266f9a954cdd4fb2ec5b7d
David Leigh <to.david.leigh@gmail.com>
2023-09-02 11:18:38 +1000

    + (Lava) Fixed an issue where some Lava comments are incorrectly shown as output.

--- Rock.Tests.Integration/Issues/Issues_v15.cs
+++ Rock.Tests.Integration/Issues/Issues_v15.cs
@@ -19,6 +19,8 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rock.Data;
 using Rock.Jobs;
+using Rock.Lava;
+using Rock.Lava.Fluid;
 using Rock.Model;
 using Rock.Tests.Integration.Core.Lava;
 using Rock.Tests.Shared;
@@ -41,7 +43,7 @@ namespace Rock.Tests.Integration.BugFixes
         /// Verifies that attempting to process an Interaction session having an empty Guid will ignore the invalid session and continue processing.
         /// </summary>
         [TestMethod]
-        public void InteractionSessionPopulateLocation_WithInteractionSessionHavingEmptyGuid_ContinuesProcessing()
+        public void InternalIssue_InteractionSessionHavingEmptyGuidCausesJobFailure()
         {
             var testInteractionGuid1 = "415EAAFD-5E32-47EA-A174-06CB46A17C4C";
             var testInteractionGuid2 = "092FD380-84CB-46B7-94F8-FC8796E67AEB";
@@ -92,6 +94,54 @@ namespace Rock.Tests.Integration.BugFixes
             var output = jobResult.OutputMessages.JoinStrings( "\n" );
             Assert.That.Contains( output, "Interaction Session with invalid Guid ignored" );
             Assert.That.Contains( output, "Updated Interaction Count And Session Duration for 1 interaction session" );
+        }
+
+        [TestMethod]
+        public void Issue5560_LavaCommentsDisplayedInOutput()
+        {
+            /* The Lava Engine may render inline comments to output where an unmatched quote delimiter is present in the preceding template text.
+             * For details, see https://github.com/SparkDevNetwork/Rock/issues/5560.
+             * 
+             * This issue is caused by the inadequacy of Regex to encapsulate the complex logic required to identify comments vs literal text.
+             * It has been fixed for the Fluid Engine by implementing shorthand comments in the custom parser.
+             * A fix for DotLiquid would require additional work to replace Regex with a custom parser to strip comments from the source template.
+             * This work is probably not justified given that DotLiquid will be removed in v17.
+             */
+
+            var engineOptions = new LavaEngineConfigurationOptions
+            {
+                InitializeDynamicShortcodes = false
+            };
+            var engine = LavaService.NewEngineInstance( typeof( FluidEngine ), engineOptions );
+
+            LavaIntegrationTestHelper.SetEngineInstance( engine );
+
+            var template = @"
+<h3>Testing issue 5560</h3>
+
+{% comment %}By Jim M...{% endcomment %}
+{% comment %}By Stan Y...{% endcomment %}
+{% comment %} By Jim M Jan 2021. This block gets a person's Explo Online group, Zoom Link, schedule, and Leader details.{% endcomment %} 
+
+/- GroupType 67 = Explo Online - assume person is in only 1 group of this type -/
+Did you see those comments ^^^
+
+{% assign groupMember = CurrentPerson | Groups: ""67"" | First %}
+{% assign grp = groupMember.Group.Id | GroupById %}
+
+            //- proceed if we found a group
+
+            {% if grp != null and grp != empty %}
+    < b > Welcome...</ b >
+{% endif %}
+";
+            var expectedOutput = @"
+<h3>Testing issue 5560</h3>
+Did you see those comments ^^^
+";
+            var actualOutput = LavaService.RenderTemplate( template ).Text;
+
+            Assert.That.AreEqualIgnoreWhitespace( expectedOutput, actualOutput );
         }
     }
 }
