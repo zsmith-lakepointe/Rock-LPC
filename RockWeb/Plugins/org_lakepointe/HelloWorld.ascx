6e26b386e47d396befeef995bfc0f647afb3a638
zsmith-lakepointe <zach.smith@lakepointe.church>
2024-10-14 16:02:22 -0500

    - Dynamic Hello World String
    Created dynamic hello world string page that is based on a user being signed in or out.

---
+++ RockWeb/Plugins/org_lakepointe/HelloWorld.ascx
@@ -0,0 +1,4 @@
+﻿ <%@ Control Language="C#" AutoEventWireup="true" CodeFile="HelloWorld.ascx.cs" Inherits="Plugins_org_lakepointe_HelloWorld" %>
+
+<h1 id="lblHello" runat="server">Hello World</h1>
+<asp:Button ID="btnChangeText" runat="server" Text="Change Text" OnClick="btnChangeText_Click" />
---
+++ RockWeb/Plugins/org_lakepointe/HelloWorld.ascx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Logical;
+using Rock;
+using Rock.Data;
+using Rock.Model;
+public partial class Plugins_org_lakepointe_HelloWorld : Rock.Web.UI.RockBlock
+{
+   
+
+protected override void OnLoad( EventArgs e )
+    {
+        base.OnLoad( e );
+
+        if ( !Page.IsPostBack )
+        {
+            var currentPerson = GetCurrentPerson();
+        }
+    }
+
+    private Person GetCurrentPerson()
+    {
+        var personId = CurrentPersonId; // This will give you the ID of the current person
+        if ( personId.HasValue )
+        {
+            using ( var rockContext = new RockContext() )
+            {
+                return new PersonService( rockContext ).Get( personId.Value );
+            }
+        }
+        return null;
+    }
+    protected void btnChangeText_Click( object sender, EventArgs e )
+    {
+        if ( CurrentPerson == null )
+        {
+            lblHello.InnerText = "Hello Stranger!";
+        }
+        else {
+            lblHello.InnerText = $"Hello {CurrentPerson.NickName}";
+        }
+        
+    }
+}
